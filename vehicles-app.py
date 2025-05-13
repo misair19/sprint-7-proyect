e56c74bf2a0b3e7a2288f07644bbb55b55f7b0f3
misair19 <misabeli619@gmail.com>
2025-05-13 17:32:44 -0500

    agrega selector de variables para visualizar datos en el gráfico

--- vehicles-app.py
+++ vehicles-app.py
@@ -10,11 +10,11 @@ import matplotlib.pyplot as plt
 vehicles_df = pd.read_csv(r'C:\PERSONAL\Data Analyst\Sprint 7\sprint-7-proyect\vehicles_us.csv')
 
 # Establecer el título
-st.title('Anuncios de ventas de vehículos en Estados Unidos')
+st.title('Anuncios de ventas de vehículos')
 
 # Establecer un encabezado
-st.header('Distribución general de precios')
-st.write('A continuación se presentan estadísticas de precios de los vehículos listados')
+st.header('Distribución general del recorrido del vehículo')
+st.write('A continuación se presentan estadísticas de trayecto acumulado de los vehículos listados')
 
 # Crear un botón
 hist_button = st.button('Construir histograma') 
@@ -22,7 +22,7 @@ hist_button = st.button('Construir histograma')
 # al hacer clic en el botón, escribir un mensaje     
 if hist_button: 
     # crear un histograma
-    fig = px.histogram(vehicles_df, x="price")
+    fig = px.histogram(vehicles_df, x="odometer")
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig)
 
@@ -51,23 +51,27 @@ df_filtrado = vehicles_df[vehicles_df['model'] == modelo_seleccionado]
 # Mostrar gráfico
 if not df_filtrado.empty:
     fig, ax = plt.subplots(figsize=(7, 3)) # crear la figura y el eje
-    sns.histplot(data=df_filtrado, x="price", hue="condition", multiple="stack", palette="light:m_r",
-    edgecolor=".3", linewidth=.5) # crear el histograma
+    sns.histplot(data=df_filtrado, x="price", hue="condition", multiple="stack", palette="deep", edgecolor=".3", linewidth=.5) # crear el histograma
     st.pyplot(fig) # mostrar el gráfico
     st.write('Datos para', modelo_seleccionado)
     st.write(df_filtrado)
 else:
     st.warning("No hay datos para ese modelo.") # advertencia si no hay datos disponibles
 
-
+st.write('Para', modelo_seleccionado, 'elija la información que desea analizar:')
 # crear una casilla de verificación
-#build_histogram = st.checkbox('Construir un histograma')
 
-# si la casilla de verificación está seleccionada
-#if build_histogram: 
- #   st.write('Construir un histograma para la columna odómetro')
-  #      ...
+option1 = st.button('Gráfico de dispersión del precio en función del recorrido') 
+option2 = st.button('Gráfico de dispersión del precio en función del año')
 
-
-
-
+# al hacer clic en el botón     
+if option1 and not option2: 
+    # crear un gráfico de dispersión
+    fig = px.scatter(df_filtrado, x="odometer", y="price", color='condition')
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig)
+elif option2 and not option1:
+    # crear un gráfico de dispersión
+    fig = px.scatter(df_filtrado, x="model_year", y="price", color='condition')
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig)
